2f862ba04dce36d0b24005bc057ac029331d6c99
Robbin16 <xuexiaoyi7758@163.com>
2023-05-26 19:42:45 +0800

    第一课

---
+++ index.js
@@ -0,0 +1,12 @@
+function validString(s) {
+  let smallLeft = "(";
+  let smallRight = ")";
+  let midLeft = "[";
+  let midRight = "]";
+  let flowerLeft = "{";
+  let flowerRight = "}";
+  let stack = [];
+  for (let char of s) {
+    char === smallLeft;
+  }
+}
